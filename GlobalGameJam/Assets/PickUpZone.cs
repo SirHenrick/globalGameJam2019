6c672dfdba687f3b2aa6812ab42a3ea2068c0d25
austinh1 <codexscript1@gmail.com>
2019-01-26 15:16:33 -0500

    Cabinets done!

--- GlobalGameJam/Assets/PickUpZone.cs
+++ GlobalGameJam/Assets/PickUpZone.cs
@@ -4,34 +4,37 @@ using UnityEngine;
 
 public class PickUpZone : MonoBehaviour
 {
-    public LayerMask layerMask;
+    public LayerMask itemLayerMask;
+    public LayerMask cabinetLayerMask;
     public Transform player;
 
     public GameObject NearestItem { get; private set; } = null;
 
     void Update()
     {
-        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(transform.position, transform.localScale, 0, layerMask);
+        Collider2D[] itemHitColliders = Physics2D.OverlapBoxAll(transform.position, transform.localScale, 0, itemLayerMask);
+        Collider2D[] cabinetHitColliders = Physics2D.OverlapBoxAll(transform.position, transform.localScale, 0, cabinetLayerMask);
 
-        int i = 0;
-        while (i < hitColliders.Length)
-        {
-            Debug.Log("Hit: " + hitColliders[i].name + i);
-            i++;
-        }
-
-        if (hitColliders.Length > 0)
-        {
-            foreach (var collider in hitColliders)
-            {
-                var position = collider.transform.position;
-                if (NearestItem == null)
-                    NearestItem = collider.gameObject;
-                else if (Vector2.Distance(position, player.position) >= Vector2.Distance(NearestItem.transform.position, player.position))
-                    NearestItem = collider.gameObject;
-            }
-        }
+        if (itemHitColliders.Length > 0)
+            NearestItem = GetNearestItem(itemHitColliders);
+        else if (cabinetHitColliders.Length > 0)
+            NearestItem = GetNearestItem(cabinetHitColliders);
         else NearestItem = null;
+    }
+
+    GameObject GetNearestItem(Collider2D[] colliders)
+    {
+        GameObject nearestItem = null;
+        foreach (var collider in colliders)
+        {
+            var position = collider.transform.position;
+            if (nearestItem == null)
+                nearestItem = collider.gameObject;
+            else if (Vector2.Distance(position, player.position) >= Vector2.Distance(nearestItem.transform.position, player.position))
+                nearestItem = collider.gameObject;
+        }
+
+        return nearestItem;
     }
 
     void OnDrawGizmos()
