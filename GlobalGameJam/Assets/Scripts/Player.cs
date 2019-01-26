edbd6c281fb42e3e6670cd35de546fe5a0178b87
austinh1 <codexscript1@gmail.com>
2019-01-26 11:55:58 -0500

    Added player, pixel perfect camera, movement works

---
+++ GlobalGameJam/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using Rewired;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    const string inputHorizontalMovement = "Horizontal Movement";
+    const string inputVerticalMovement = "Vertical Movement";
+
+    Rewired.Player player;
+    float horizontalAxis = 0f;
+    float verticalAxis = 0f;
+
+    // Attributes
+    float speed = 10f;
+
+    Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = ReInput.players.GetSystemPlayer();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = player.GetAxis(inputHorizontalMovement);
+        verticalAxis = player.GetAxis(inputVerticalMovement);
+
+        if (horizontalAxis != 0)
+            Debug.Log(horizontalAxis);
+
+        if (verticalAxis != 0)
+            Debug.Log(verticalAxis);
+
+        body.velocity = new Vector2(horizontalAxis, verticalAxis).normalized * speed;
+    }
+}
---
+++ GlobalGameJam/Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{
+    float ppu = 16; // pixels per unit (your tile size)
+
+    private void LateUpdate()
+    {
+        Vector3 position = transform.localPosition;
+
+        position.x = (Mathf.Round(transform.parent.position.x * ppu) / ppu) - transform.parent.position.x;
+        position.y = (Mathf.Round(transform.parent.position.y * ppu) / ppu) - transform.parent.position.y;
+
+        transform.localPosition = position;
+    }
+}
