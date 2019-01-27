2ad0da56af642549fdea7c6a4f2f4a2f7be8b6ad
JDGifford <darkblaze53@yahoo.com>
2019-01-27 15:08:10 -0500

    Added Kid Crowd outside door

---
+++ GlobalGameJam/Assets/KidCrowd.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidCrowd : MonoBehaviour
+{
+    float timer = 0;
+    public float timeLimit = 0.2f;
+    bool state = false;
+
+    public SpriteRenderer spriteRenderer;
+
+    public Sprite firstSprite;
+    public Sprite secondSprite;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeLimit)
+        {
+            timer = 0;
+            state = !state;
+        }
+
+        if (state)
+        {
+            spriteRenderer.sprite = firstSprite;
+        }else
+        {
+            spriteRenderer.sprite = secondSprite;
+        }
+
+    }
+}
