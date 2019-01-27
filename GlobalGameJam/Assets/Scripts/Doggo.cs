3ab4f732e57fbaac93b1aa210bac6cf2530c7de2
Andy G <andiecanjr@gmail.com>
2019-01-27 15:18:04 -0500

    Add Doggos!

--- GlobalGameJam/Assets/Scripts/Doggo.cs
+++ GlobalGameJam/Assets/Scripts/Doggo.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Doggo : MonoBehaviour
 {
     private Rigidbody2D body;
+
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         var startingDirection = new Vector2(Random.value, Random.value);
         startingDirection.Normalize();
 
@@ -19,5 +22,6 @@ public class Doggo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        spriteRenderer.transform.localScale =  new Vector2(Mathf.Sign(body.velocity.x), 1f);
     }
 }
